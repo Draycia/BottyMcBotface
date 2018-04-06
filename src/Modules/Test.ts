e66bcca99f93900e89752feb32fa48e66966d8b5
Sir-Kam <kamuisamaa@gmail.com>
2018-04-05 22:54:00 -0400

    Renamed from 'HelloWorld'
    Added 2 public fields also for cleaner logging on `init()` and
    `deinit()`

---
+++ src/Modules/Test.ts
@@ -0,0 +1,24 @@
+import Discord = require("discord.js");
+import CallerDataObject from "./Command";
+
+export default class Test {
+  private bot: Discord.Client;
+
+  public readonly fileName = __filename.substr(__filename.lastIndexOf('\\') + 1, __filename.lastIndexOf('.'));
+  public readonly className = this.constructor.name;
+  public init(obj: any) {
+    if (!obj.bot) console.log("Bot is null :(");
+    console.log(`'${this.className}' module loaded from file '${this.fileName}'!`);
+    console.log("Test module loaded!");
+
+    this.bot = obj.bot;
+  }
+
+  public deinit() {
+    console.log(`'${this.className}' module unloaded from file '${this.fileName}'!`);
+  }
+
+  public testCommand(callerDataObj: CallerDataObject) {
+    callerDataObj.message.channel.send("Test! Args: " + callerDataObj.args.toString());
+  }
+}
